aba6aa8e768efb4a73087bfbd2d367d37c786d73
Jeremy Elson <jelson@gmail.com>
2021-12-13 14:44:03 -0800

    add send udp script

---
+++ send_udp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+NUM_PACKETS=100
+PACKET_LEN=1400
+
+import socket
+import time
+import random
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+nonce = f"cmdline-exp-{time.time()}"
+for i in range(NUM_PACKETS):
+    header = f"magicheader:{nonce}:{i}:{NUM_PACKETS}\n"
+    payload_len = PACKET_LEN - len(header)
+    payload = "".join([str(random.randrange(0, 10)) for i in range(payload_len)])
+    data = (header + payload).encode('utf-8')
+    sock.sendto(data, ("www.megabozo.com", 7777))
+
+            
