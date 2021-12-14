69d0370ec5e3f96ceedcee929f7eb390b2bd7f80
Jeremy Elson <jelson@gmail.com>
2021-12-13 17:04:41 -0800

    send redundantly

--- send_udp.py
+++ send_udp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 NUM_PACKETS=100
+NUM_REPEATS=3
 PACKET_LEN=1400
 
 import socket
@@ -10,11 +11,11 @@ import random
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 nonce = f"cmdline-exp-{time.time()}"
-for i in range(NUM_PACKETS):
-    header = f"magicheader:{nonce}:{i}:{NUM_PACKETS}\n"
-    payload_len = PACKET_LEN - len(header)
-    payload = "".join([str(random.randrange(0, 10)) for i in range(payload_len)])
-    data = (header + payload).encode('utf-8')
-    sock.sendto(data, ("www.megabozo.com", 7777))
 
-            
+for cnt in range(NUM_REPEATS):
+   for i in range(NUM_PACKETS):
+       header = f"magicheader:{nonce}:{i}:{NUM_PACKETS}\n"
+       payload_len = PACKET_LEN - len(header)
+       payload = "".join([str(random.randrange(0, 10)) for i in range(payload_len)])
+       data = (header + payload).encode('utf-8')
+       sock.sendto(data, ("www.megabozo.com", 7777))
